0d12752a004395f037f26a778f67b94efad64805
Igor Fernandes <igor.fernandes95@outlook.com>
2024-04-29 15:01:10 -0300

    Testes realizados, busca byId feito

--- test/usuario.e2e-spec.ts
+++ test/usuario.e2e-spec.ts
@@ -49,4 +49,63 @@ describe('Testes dos Módulos Usuário e Auth (e2e)', () => {
 
   })
 
-});
+  it('02 - Deve Autenticar Usuario (Login)', async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/usuarios/logar')
+      .send({
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+      });
+    expect(200)
+
+    token = resposta.body.token;
+
+  });
+
+  it('03 - Não Deve Duplicar o Usuário', async () => {
+    return request(app.getHttpServer())
+      .post('/usuarios/cadastrar')
+      .send({
+        nome: 'Root',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+        foto: ' '
+      })
+      .expect(400)
+  });
+
+  it('04 - Deve Listar todos os Usuários', async () => {
+    return request(app.getHttpServer())
+      .get('/usuarios/all')
+      .set('Authorization', `${token}`)
+      .send({})
+      .expect(200)
+  });
+
+  it('05 - Deve Atualizar um Usuário', async () => {
+    return request(app.getHttpServer())
+      .put('/usuarios/atualizar')
+      .set('Authorization', `${token}`)
+      .send({
+        id: usuarioId,
+        nome: 'Root Atualizado',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+        foto: ' '
+      })
+      .expect(200)
+      .then(resposta => {
+        expect("Root Atualizado").toEqual(resposta.body.nome);
+      });
+  });
+
+  it('06 - Deve retornar um usuario por id', async () => {
+    return request(app.getHttpServer())
+      .get(`/usuarios/${usuarioId}`)
+      .set('Authorization', `${token}`)
+      .send({})
+      .expect(200)
+    
+  });
+
+})
