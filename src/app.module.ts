d8e0168034b6a8d4d2f18abd649a6213a1b57391
Igor Fernandes <igor.fernandes95@outlook.com>
2024-04-30 14:31:33 -0300

    Apaguei as importações que n estava usando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tema } from './tema/entities/tema.entity';
-import { Usuario } from './auth/usuario/entities/usuario.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './auth/usuario/usuario.module';
 import { PostagemModule } from './postagem/postagem.module';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
-import { DevService } from './data/services/dev.service';
+
 
 
 @Module({
